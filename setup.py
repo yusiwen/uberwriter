913f45930db85d2cba242f29b0b368e3245e191b
Wolf Vollprecht <w.vollprecht@googlemail.com>
2012-07-15 03:08:22 +0200

    commit before release

--- setup.py
+++ setup.py
@@ -87,16 +87,16 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='uberwriter',
-    version='12.07.6',
+    version='12.07.7',
     license='GPL-3',
     author='Wolf Vollprecht',
     author_email='w.vollprecht@gmail.com',
     description='UberWriter, beautiful distraction free writing',
     long_description='With UberWriter you get only one thing: An empty textbox, that is to fill with your ideas. There are no settings, you don\'t have to choose a font, it is only for writing.\
  You can use markdown for all your markup needs.\
- PDF, RTF and HTML are generated with pandoc. You find more information about the markdown syntax at their homepage.\
-    url='https://launchpad.net/uberwriter',
- To use PDF export consider installing the optional LaTeX package (texlive-luatex) from the software center.',
+ PDF, RTF and HTML are generated with pandoc. You find more information about the markdown syntax at their homepage: http://johnmacfarlane.net/pandoc/README.html.\
+ \
+ For PDF generation it is also required that you choose to install the texlive-luatex package.',
     url='https://launchpad.net/uberwriter',
     cmdclass={'install': InstallAndUpdateDataDirectory}
     )
