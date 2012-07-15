831f360d730c66f70ba1e78d542118f9903fcd4d
Wolf Vollprecht <w.vollprecht@googlemail.com>
2012-07-15 03:11:26 +0200

    commit before release

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='uberwriter',
-    version='12.07.10',
+    version='12.07.11',
     license='GPL-3',
     author='Wolf Vollprecht',
     author_email='w.vollprecht@gmail.com',
