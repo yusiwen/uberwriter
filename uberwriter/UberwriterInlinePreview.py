3316d3e5a729a0038970ff2b243fe9ca941d4d2e
Wolf Vollprecht <w.vollprecht@googlemail.com>
2012-09-27 20:32:51 +0200

    added files for inline preview, sorry about that.

---
+++ uberwriter/UberwriterInlinePreview.py
@@ -0,0 +1,44 @@
+from gi.repository import Gtk
+from uberwriter_lib import LatexToPNG
+class UberwriterInlinePreview():
+	
+	def __init__(self, view, text_buffer):
+		self.TextView = view
+		self.TextBuffer = text_buffer
+		self.LatexConverter = LatexToPNG.LatexToPNG()
+		cursor_mark = self.TextBuffer.get_insert()
+		cursor_iter = self.TextBuffer.get_iter_at_mark(cursor_mark)
+		self.ClickMark = self.TextBuffer.create_mark('click', cursor_iter)
+		# Events for popup menu
+		self.TextView.connect_after('populate-popup', self.populate_popup)
+		self.TextView.connect_after('popup-menu', self.move_popup)
+		self.TextView.connect('button-press-event', self.click_move_button)
+
+	def click_move_button(self, widget, event):
+		if event.button == 3:
+			x, y = self.TextView.window_to_buffer_coords(2, int(event.x), int(event.y))
+			self.TextBuffer.move_mark(self.ClickMark, self.TextView.get_iter_at_location(x, y))
+
+	def populate_popup(self, editor, menu,  data=None):
+		item = Gtk.MenuItem.new()
+
+		start_iter = self.TextBuffer.get_iter_at_mark(self.ClickMark)
+		end_iter = start_iter.copy()
+		start_iter.set_line_offset(0)
+		end_iter.forward_to_line_end()
+
+		text = self.TextBuffer.get_text(start_iter, end_iter, False)
+		print text
+		latex_image = self.LatexConverter.generatepng('$\\frac{d}{dx}\\left( \\int_{0}^{x} f(u)\\,du\\right)=f(x).$')
+
+		image = Gtk.Image.new_from_file(latex_image)
+		image.show()
+		item.add(image)
+		item.set_property('width-request', 50)
+		item.show()
+		print menu, item
+		menu.prepend(item)
+		menu.show()
+
+	def move_popup(self):
+		pass
---
+++ uberwriter_lib/LatexToPNG.py
@@ -0,0 +1,81 @@
+"""
+Based on latex2png.py from Stuart Rackham
+
+AUTHOR
+	Written by Stuart Rackham, <srackham@gmail.com>
+	The code was inspired by Kjell Magne Fauske's code:
+	http://fauskes.net/nb/htmleqII/
+
+	See also:
+	http://www.amk.ca/python/code/mt-math
+	http://code.google.com/p/latexmath2png/
+
+COPYING
+	Copyright (C) 2010 Stuart Rackham. Free use of this software is
+	granted under the terms of the MIT License.
+"""
+
+import os, sys, tempfile, md5
+
+class LatexToPNG():
+
+	TEX_HEADER = r'''\documentclass{article}
+	\usepackage{amsmath}
+	\usepackage{amsthm}
+	\usepackage{amssymb}
+	\usepackage{bm}
+	\newcommand{\mx}[1]{\mathbf{\bm{#1}}} % Matrix command
+	\newcommand{\vc}[1]{\mathbf{\bm{#1}}} % Vector command
+	\newcommand{\T}{\text{T}}			 % Transpose
+	\pagestyle{empty}
+	\begin{document}'''
+	
+	TEX_FOOTER = r'''\end{document}'''
+
+	def __init__(self):
+		self.temp_result = tempfile.NamedTemporaryFile(suffix='.png')
+
+	def run(self, cmd):
+		cmd += ' >/dev/null 2>&1'
+		if os.system(cmd):
+			raise 'failed command: %s' % cmd
+
+
+	def latex2png(self, tex, outfile, dpi, modified):
+		'''Convert LaTeX input file infile to PNG file named outfile.'''
+		outfile = os.path.abspath(outfile)
+		outdir = os.path.dirname(outfile)
+		#if not os.path.isdir(outdir):
+		#	raise EApp, 'directory does not exist: %s' % outdir
+		texfile = tempfile.mktemp(suffix='.tex', dir=os.path.dirname(outfile))
+		basefile = os.path.splitext(texfile)[0]
+		dvifile = basefile + '.dvi'
+		temps = [basefile + ext for ext in ('.tex','.dvi', '.aux', '.log')]
+		skip = False
+		
+		tex = '%s\n%s\n%s\n' % (self.TEX_HEADER, tex.strip(), self.TEX_FOOTER)
+
+		open(texfile, 'w').write(tex)
+		saved_pwd = os.getcwd()
+
+		os.chdir(outdir)
+		try:
+			# Compile LaTeX document to DVI file.
+			self.run('latex %s' % texfile)
+			# Convert DVI file to PNG.
+			cmd = 'dvipng'
+			if dpi:
+				cmd += ' -D %s' % dpi
+			cmd += ' -T tight -x 1000 -z 9 -bg Transparent -o "%s" "%s"' \
+					% (outfile,dvifile)
+			self.run(cmd)
+		finally:
+			os.chdir(saved_pwd)
+		for f in temps:
+			if os.path.isfile(f):
+				os.remove(f)
+
+	def generatepng(self, formula):
+		self.temp_result = tempfile.NamedTemporaryFile(suffix='.png')
+		self.latex2png(formula, self.temp_result.name, 300, False)
+		return self.temp_result.name
